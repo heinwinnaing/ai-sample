19edf15640f0946110846051384686fe4639eb71
Hein Win Naing <heinwinnaing@gmail.com>
2025-04-03 12:24:15 +0700

    #sample ai console app

---
+++ AI.ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+try 
+{
+    //docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:latest
+    //docker exec -it ollama ollama run llama3
+
+    var builder = Host.CreateApplicationBuilder();
+    builder.Services
+        .AddChatClient(new OllamaChatClient(endpoint: new Uri("http://localhost:11434"), modelId: "llama3"));
+
+    var app = builder.Build();
+    var chatClient = app.Services.GetRequiredService<IChatClient>();
+    
+    bool chatting = true;
+    List<ChatMessage> messages = new();
+    while (chatting)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow ;
+        Console.Write($"Prompt: ");
+        var input = Console.ReadLine()!;
+        messages.Add(new ChatMessage(ChatRole.User, input));
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        var response = await chatClient.GetResponseAsync(messages);
+        await foreach(var result in chatClient.GetStreamingResponseAsync(new ChatMessage(ChatRole.User, input)))
+        {
+            Console.Write(result.Text);
+        }
+        Console.WriteLine(Environment.NewLine);
+    }
+}
+catch(Exception ex) 
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{ex.Message} {ex.InnerException?.Message}");
+}
